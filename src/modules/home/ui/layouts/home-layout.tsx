3c413c687dcb3cbccc131a3f6e04fec873c5eb76
WebApp07 <aminezouguig@proton.me>
2025-02-12 16:47:01 -0800

    Basic Layout, Sidebar component Sidebar sections sidebar items

---
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { HomeNavbar } from "../components/home-navbar";
+import { HomeSidebar } from "../components/home-sidebar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <SidebarProvider>
+      <div className="w-full">
+        <HomeNavbar />
+      </div>
+      <div className="flex min-h-screen pt-[4rem]">
+        <HomeSidebar />
+        <main className="flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
+};
+export default Layout;
