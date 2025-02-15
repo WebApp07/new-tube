f7850d3da2537b3ad5f57d03033aa00e47825402
WebApp07 <aminezouguig@proton.me>
2025-02-15 03:00:21 +0100

    TRPC setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>I will load videos in the future</div>;
+import { trpc } from "@/trpc/client";
+
+export default async function Home() {
+  const { data } = trpc.hello.useQuery({ text: "Amine World!" });
+
+  return <div>Client component says: {data?.greeting}</div>;
 }
