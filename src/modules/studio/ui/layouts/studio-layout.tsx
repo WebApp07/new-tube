f32ec3655da2d2ee35978b5a3ec171124e2e9e4c
WebApp07 <aminezouguig@proton.me>
2025-02-24 12:10:26 +0000

    Create studio route group, Create studio layout, Protect studio routes

--- src/modules/studio/ui/layouts/studio-layout.tsx
+++ src/modules/studio/ui/layouts/studio-layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { HomeNavbar } from "../components/home-navbar";
-import { HomeSidebar } from "../components/home-sidebar";
+import { StudioNavbar } from "../components/studio-navbar";
+import { StudioSidebar } from "../components/studio-sidebar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,10 +10,10 @@ const StudioLayout = ({ children }: LayoutProps) => {
   return (
     <SidebarProvider>
       <div className="w-full">
-        <HomeNavbar />
+        <StudioNavbar />
       </div>
       <div className="flex min-h-screen pt-[4rem]">
-        <HomeSidebar />
+        <StudioSidebar />
         <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
     </SidebarProvider>
